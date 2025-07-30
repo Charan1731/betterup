00b40c2a8a4944a66d0aa5ebe7ecdc8fc6f713d7
Charan1731 <charan23114@gmail.com>
2025-07-30 21:58:24 +0530

    added new file

--- apps/frontend/utils/Data.tsx
+++ apps/frontend/utils/Data.tsx
@@ -1,44 +1,58 @@
 export const trendingWebsites = [
+  // Global & Regional Essentials
   { name: "Google", url: "https://www.google.com" },
   { name: "YouTube", url: "https://www.youtube.com" },
   { name: "Instagram", url: "https://www.instagram.com" },
   { name: "WhatsApp", url: "https://www.whatsapp.com" },
   { name: "ChatGPT", url: "https://chatgpt.com" },
-  { name: "Facebook", url: "https://www.facebook.com" },                 // Added
+  { name: "Facebook", url: "https://www.facebook.com" },
+  { name: "Amazon", url: "https://www.amazon.com" },
   { name: "Amazon India", url: "https://www.amazon.in" },
-  { name: "Amazon", url: "https://www.amazon.com" },                     // Added
   { name: "Reddit", url: "https://www.reddit.com" },
   { name: "Netflix", url: "https://www.netflix.com" },
-  { name: "TikTok", url: "https://www.tiktok.com" },                     // Added
-  { name: "Yahoo", url: "https://www.yahoo.com" },                       // Added
-  { name: "Wikipedia", url: "https://www.wikipedia.org" },               // Added
+  { name: "Wikipedia", url: "https://www.wikipedia.org" },
   { name: "LinkedIn", url: "https://www.linkedin.com" },
   { name: "Spotify", url: "https://www.spotify.com" },
-  { name: "Pinterest", url: "https://www.pinterest.com" },
   { name: "Telegram", url: "https://t.me" },
-  { name: "Steam", url: "https://store.steampowered.com" },
-  { name: "Threads", url: "https://www.threads.net" },
-  { name: "Snapchat", url: "https://www.snapchat.com" },
-  { name: "Twitch", url: "https://www.twitch.tv" },
-  { name: "NYTimes", url: "https://www.nytimes.com" },
-  { name: "BBC", url: "https://www.bbc.com" },
-  { name: "The Guardian", url: "https://www.theguardian.com" },
-  { name: "CNN", url: "https://www.cnn.com" },                           // Added
-  { name: "Reuters", url: "https://www.reuters.com" },                   // Added
-  { name: "MSN", url: "https://www.msn.com" },                           // Added
   { name: "Flipkart", url: "https://www.flipkart.com" },
   { name: "Indian Express", url: "https://www.indianexpress.com" },
   { name: "Hindustan Times", url: "https://www.hindustantimes.com" },
   { name: "NDTV", url: "https://www.ndtv.com" },
   { name: "IRCTC", url: "https://www.irctc.co.in" },
-  { name: "Bing", url: "https://www.bing.com" },                         // Added
-  { name: "DuckDuckGo", url: "https://www.duckduckgo.com" },             // Added
-  { name: "Temu", url: "https://www.temu.com" },                         // Added - trending ecommerce
-  { name: "Yahoo Japan", url: "https://www.yahoo.co.jp" },               // Added
-  { name: "Baidu", url: "https://www.baidu.com" },                       // Added - China search engine
-  { name: "Naver", url: "https://www.naver.com" },                       // Added - Korea
-  { name: "Canva", url: "https://www.canva.com" },                       // Added - design tool
-  { name: "Roblox", url: "https://www.roblox.com" },                     // Added - gaming
-  { name: "Fandom", url: "https://www.fandom.com" },                     // Added - entertainment
-  { name: "Office", url: "https://www.office.com" }                      // Added - productivity
+  { name: "Bing", url: "https://www.bing.com" },
+  { name: "DuckDuckGo", url: "https://www.duckduckgo.com" },
+
+  // Major Crypto Exchanges & Info
+  { name: "Binance", url: "https://www.binance.com" },                // Top exchange[2][20]
+  { name: "Coinbase", url: "https://www.coinbase.com" },               // Top for beginners[4]
+  { name: "Kraken", url: "https://www.kraken.com" },                   // Global exchange[4]
+  { name: "Crypto.com", url: "https://crypto.com" },                   // Leading mobile-first and staking[1]
+  { name: "CoinMarketCap", url: "https://coinmarketcap.com" },         // Crypto prices & stats[2]
+  { name: "CoinGecko", url: "https://www.coingecko.com" },             // Similar to CMC[20]
+  { name: "WazirX", url: "https://wazirx.com" },                       // Indian platform[11]
+  { name: "Gemini", url: "https://www.gemini.com" },                   // Crypto exchange[4]
+  { name: "Uniswap", url: "https://app.uniswap.org" },                 // Major DEX[6][12]
+  { name: "Aave", url: "https://app.aave.com" },                       // Leading DeFi lending[6][12]
+  { name: "PancakeSwap", url: "https://pancakeswap.finance" },         // BSC DEX[12]
+  { name: "MakerDAO", url: "https://makerdao.com" },                   // Stablecoin lending[12]
+  { name: "Curve", url: "https://curve.fi" },                          // Stablecoin DEX[12]
+  { name: "SushiSwap", url: "https://sushi.com" },                     // Multichain DEX[12]
+  { name: "dYdX", url: "https://dydx.exchange" },                      // Perpetual trading DEX[9]
+  { name: "Compound", url: "https://compound.finance" },               // DeFi lending[19]
+  { name: "DappRadar", url: "https://dappradar.com" },                 // Track top DApps[13]
+  { name: "Alchemy Dapps Directory", url: "https://www.alchemy.com/dapps" }, // DApp listings[3][7][9]
+  { name: "DeBank", url: "https://debank.com" },                       // DeFi portfolio tracker
+  { name: "Lens Protocol", url: "https://www.lens.xyz" },              // Web3 Social[7]
+  { name: "Bluesky", url: "https://bsky.app" },                        // Decentralized Social[7]
+  { name: "Mirror.xyz", url: "https://mirror.xyz" },                   // Web3 blogging/NFTs[7]
+  { name: "NEAR Protocol", url: "https://www.near.org" },              // Leading DApp chain[10]
+  { name: "The Graph", url: "https://thegraph.com" },                  // Web3 data Infrastructure[10]
+  { name: "CoinDesk", url: "https://www.coindesk.com" },               // Major crypto news[8]
+  { name: "Decrypt", url: "https://decrypt.co" },                      // Trusted news/learning[8]
+  { name: "The Block", url: "https://www.theblock.co" },               // News & research[8]
+  { name: "Bankless", url: "https://www.bankless.com" },               // DeFi & open finance[8]
+  { name: "BeInCrypto", url: "https://beincrypto.com" },               // News & guides[8]
+  { name: "MetaMask", url: "https://metamask.io" },                    // Leading DeFi wallet
+  { name: "ZenGo", url: "https://zengo.com" },                         // User-friendly DeFi wallet[15]
+  { name: "Best Wallet", url: "https://bestwallet.com" }               // Popular for beginners[15]
 ];
